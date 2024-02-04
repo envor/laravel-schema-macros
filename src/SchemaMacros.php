c88ce8e58c04afdcf1d3a4a03dae67baeea5166f
= <inman.turbo@gmail.com>
2024-02-04 15:27:53 -0500

    php8.2

--- src/SchemaMacros.php
+++ src/SchemaMacros.php
@@ -4,5 +4,10 @@ namespace Envor\SchemaMacros;
 
 class SchemaMacros
 {
-    public const string TRASH_DATE_FORMAT = 'Y-m-d_H-i-s';
+    /**
+     * The date format used for the trash table
+     * 
+     * @var string
+     */
+    public const TRASH_DATE_FORMAT = 'Y-m-d_H-i-s';
 }
